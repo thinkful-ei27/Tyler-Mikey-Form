528578c29d7068cfbfc2f6fa0aa7d6e09b84fb7e
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-01-31 15:00:14 -0500

    finished validation, made api request, synchronized name fields, tested error and success messages

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,110 @@
 import React from 'react';
-import {reduxForm, Field} from 'redux-form';
+import {reduxForm, SubmissionError, Field} from 'redux-form';
 import './form.css'
 import {required, nonEmpty, lengthVal, numVal} from '../validators'
+import Input from './Input'
 export class Form extends React.Component{
 
+	onSubmit(values){
+		console.log(values)
+		return fetch(
+			'https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+				method : 'POST',
+				body:JSON.stringify(values),
+				headers:{'Content-Type':'application/json'}
+			})
+			.then(res => {
+				if (!res.ok) {
+						if (
+								res.headers.has('content-type') &&
+								res.headers
+										.get('content-type')
+										.startsWith('application/json')
+						) {
+								// It's a nice JSON error returned by us, so decode it
+								return res.json().then(err => Promise.reject(err));
+						}
+						// It's a less informative error returned by express
+						return Promise.reject({
+								code: res.status,
+								message: res.statusText
+						});
+				}
+				return;
+		})
+		.then(() => console.log('Submitted with values', values))
+		.catch(err => {
+				const {reason, message, location} = err;
+				if (reason === 'ValidationError') {
+						// Convert ValidationErrors into SubmissionErrors for Redux Form
+						return Promise.reject(
+								new SubmissionError({
+										[location]: message
+								})
+						);
+				}
+				return Promise.reject(
+						new SubmissionError({
+								_error: 'Error submitting message'
+						})
+				);
+		});
+	}
+
+
+
     render(){
+			let successMessage;
+			if (this.props.submitSucceeded) {
+					successMessage = (
+							<div className="message message-success">
+									Message submitted successfully
+							</div>
+					);
+			}
+
+			let errorMessage;
+			if (this.props.error) {
+					errorMessage = (
+							<div className="message message-error">{this.props.error}</div>
+					);
+					}
         return (
+				
             <div className="delivery-form">
-            <form>
+            <form onSubmit={this.props.handleSubmit((values)=>this.onSubmit(values))}>
                 <h1>Report a problem with your delivery</h1>
+								{successMessage}
+								{errorMessage}
                 <label>Tracking number</label>
                 <Field
-                name="tracking number"
+                name="trackingNumber"
                 type="text"
-                component='input'
-                validate={[required, nonEmpty, lengthVal]}
+                component={Input}
+                validate={[required, nonEmpty, lengthVal, numVal]}
                 />
                 
                 <label>What is your issue</label>
-                <Field 
-                name="Options"
+								<Field 
+								component={Input}
+								validate={required}
+                name="issue"
                 type="select"
-                component='select'>
-                <option>my delivery hasnt arrived</option>
-                <option>The wrong item was delivered</option>
-                <option>part of my order was missing</option>
-                <option>my order was damaged</option>
-                <option>Other (Provide details below)</option>
+                element="select"
+								id="select"
+                >
+                  <option value = "not-delivered">my delivery hasnt arrived</option>
+                    <option value="wrong-item">The wrong item was delivered</option>
+                    <option value="missing-part">part of my order was missing</option>
+                    <option value="damaged">my order was damaged</option>
+                    <option value="other">Other (Provide details below)</option>
                 </Field>
                 <label>Give us more details (optional)</label>
                 <Field
                 name='details'
                 type='text'
-                component='textarea'
+                element="textarea"
+                component={Input}
                 />
                 <button type="submit">Submit</button>
             </form>
--- src/validators.js
+++ src/validators.js
@@ -4,10 +4,13 @@ export const nonEmpty = value =>
 
 export const lengthVal = value => (value.trim().length === 5? undefined: 'Tracking number must be 5 digits long')
 
-export const numVal = (value) => {
-    let arr= []
-    for(let i = 0; i < value.length; i++){
+
+export const numVal =value => value && isNaN(Number(value)) ? 'Must be a number' : undefined;
+
+// export const numVal = (value) => {
+//     let arr= []
+//     for(let i = 0; i < value.length; i++){
     
-    }
-    return undefined
-}
+//     }
+//     return undefined
+// }
